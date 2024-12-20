59cabf094396d32e5881c0801e6751b1e151a049
Shubham Danu <danushubham18@gmail.com>
2024-12-20 22:28:23 +0530

    chnage styleing

--- script/script.js
+++ script/script.js
@@ -3,3 +3,22 @@ function toggleMenu(){
     
     menu.classList.toggle("active");
 }
+
+
+// header run
+
+const headerRun = document.querySelector("#headerRun")
+
+let str = "Hey, My name is Shubham Danu."
+
+let i = 0;
+
+setInterval(() => {
+    if (str.length > i){
+        headerRun.textContent += str[i]
+    } else {
+        headerRun.textContent = ""
+        i = -1
+    }
+    i++
+},100)
